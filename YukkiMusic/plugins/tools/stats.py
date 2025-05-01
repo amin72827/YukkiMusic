e00a0e54c810d704bba7310f7aeb670d20f92616
﮼محمد <saroksurarazo@gmail.com>
2025-05-01 17:08:15 +0300

    Update stats.py
    Signed-off-by: ﮼محمد <saroksurarazo@gmail.com>

--- YukkiMusic/plugins/tools/stats.py
+++ YukkiMusic/plugins/tools/stats.py
@@ -8,14 +8,6 @@
 # All rights reserved.
 #
 
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-
 import asyncio
 import platform
 from sys import version as pyver
