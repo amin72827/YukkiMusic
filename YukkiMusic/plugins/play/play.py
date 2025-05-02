99a6a837f3eb0e9a981bd2a1b4cbbed223ea41df
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-05-02 15:16:07 +0530

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -61,6 +61,10 @@ async def play_commnd(
         file = rmsg.file
         audio_telegram = True if (rmsg.audio or rmsg.voice) else False
         video_telegram = True if rmsg.video else False
+        if not audio_telegram and not video_telegram and rmsg.document:
+            if rmsg.document.mime_type.startswith('video'):
+                video_telegram = True
+            
 
     if audio_telegram:
         if file.size > config.TG_AUDIO_FILESIZE_LIMIT:
