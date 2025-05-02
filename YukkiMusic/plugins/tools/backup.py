b889eb6eeebad910c4d495e0d356b61f33c633a3
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-05-02 16:51:49 +0530

    update YukkiMusic/plugins/tools/backup.py

--- YukkiMusic/plugins/tools/backup.py
+++ YukkiMusic/plugins/tools/backup.py
@@ -157,7 +157,8 @@ async def import_database(event):
         try:
             await mystic.edit(f"Downloading... {current * 100 / total:.1f}%")
         except FloodWaitError as e:
-            await asyncio.sleep(e.seconds)
+            #await asyncio.sleep(e.seconds)
+            c
 
     file_path = await reply.download_media(progress_callback=progress)
 
