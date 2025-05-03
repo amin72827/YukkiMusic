43376a6bd9c6b54c1413c8ee253a008cf663af26
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-05-03 06:42:18 +0530

    update YukkiMusic/core/track.py

--- YukkiMusic/core/track.py
+++ YukkiMusic/core/track.py
@@ -124,10 +124,7 @@ class Track:
             @asyncify
             def _download():
                 with YoutubeDL(ytdl_opts) as ydl:
-                    try:
-                        info = ydl.extract_info(self.download_url, False)
-                    except Exception as e:
-                        raise e
+                    info = ydl.extract_info(self.download_url, False)
                     self.file_path = os.path.join(
                         "downloads", f"{info['id']}.{info['ext']}"
                     )
